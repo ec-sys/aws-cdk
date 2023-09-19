1ebdc45e1be5183bbca4a6da7df97f12e9e54a34
longdangdjv <long.dang@drjoy.vn>
2023-09-19 02:38:34 +0000

    update

---
+++ todo-app/src/main/java/com/myorg/todo/config/CustomConfiguration.java
@@ -0,0 +1,9 @@
+package com.myorg.todo.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(CustomConfigurationProperties.class)
+class CustomConfiguration {
+}
