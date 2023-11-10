6079d210d00e855d05454f7943a3d47846626570
longdangdjv <long.dang@drjoy.vn>
2023-11-10 10:32:06 +0000

    update

--- lambda/todo-auth/index.js
+++ lambda/todo-auth/index.js
@@ -1,11 +1,18 @@
-import {validateToken} from "./services/auth.service.js";
-import {createTestAccessToken} from "./utils/redis.util.js";
+import jwt from 'jsonwebtoken';
+import {createClient} from 'redis';
+import {SSMClient, GetParameterCommand} from '@aws-sdk/client-ssm';
+
+const ssmClient = new SSMClient();
 
 export const handler = async (event, context) => {
-  const accessToken = await createTestAccessToken();
-  console.log(accessToken);
+  const privateKey = await getParameterByName('TODO_APP_ACCESS_TOKEN_SECRET', false);
+  const redisHost = await getParameterByName('TODO_APP_REDIS_HOST', false);
+  console.log(privateKey);
+  console.log(redisHost);
 
-  const checkTokenResult = await validateToken(accessToken);
+  const accessToken = await createTestAccessToken(redisHost, privateKey);
+  console.log(accessToken);
+  const checkTokenResult = await validateToken(accessToken, redisHost, privateKey);
   console.log(checkTokenResult);
 
   const response = {
@@ -14,3 +21,98 @@ export const handler = async (event, context) => {
   };
   return response;
 };
+
+async function validateToken(token, redisHost, privateKey) {
+  let response = {
+    success: true,
+    message: ''
+  }
+
+  // null or empty token
+  if (!token) {
+    response.success = false;
+    response.message = 'Empty token';
+    return response;
+  }
+
+  // validate by jwt
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, privateKey);
+  } catch (err) {
+    response.success = false;
+    console.log(err);
+    return response;
+  }
+
+  // validate by check in redis
+  if(!await validateTokenInRedis(decodedToken, redisHost)) {
+    response.success = false;
+    response.message = 'Invalid token';
+    return response;
+  }
+  return response;
+}
+
+async function validateTokenInRedis(decodedToken, redisHost) {
+  const redisClient = createClient({url: 'redis://' + redisHost});
+  await redisClient.connect();
+
+  const tokenId = decodedToken.jti;
+  const accessToken = await redisClient.get(tokenId);
+
+  // validate exist by token id
+  if(!accessToken) return false;
+
+  return true;
+}
+
+async function getParameterByName(parameterName, isDecrypt) {
+  const profile = process.env.PROFILE;
+  // locally evn
+  if(!profile || profile == 'local') {
+    return getParameterInEnvLocal(parameterName);
+  }
+
+  // lambda evn
+  const params = {
+    Name: parameterName,
+    WithDecryption: isDecrypt
+  };
+  const command = new GetParameterCommand(params);
+  const response = await ssmClient.send(command);
+  return response.Parameter.Value;
+}
+
+function getParameterInEnvLocal(parameterName) {
+  if(parameterName == 'TODO_APP_ACCESS_TOKEN_SECRET') {
+    return 'drjoy1115';
+  }
+  if(parameterName == 'TODO_APP_REDIS_HOST') {
+    return 'localhost:6379';
+  }
+  throw 'Invalid parameter name: ' + parameterName;
+}
+
+async function createTestAccessToken(redisHost, privateKey) {
+  const tokenId = '133b2bff-e32f-4a5b-9ba4-f84c40ccef96';
+  const payload = {
+    userInfo: {
+      id: 1,
+      name: 'longdd90',
+      email: 'long.dang@drjoy.vn'
+    },
+    jti: tokenId
+  }
+
+  const accessToken = jwt.sign(payload, privateKey, {
+    algorithm: 'HS256',
+    expiresIn: '1h'
+  });
+
+  const redisClient =  createClient({url: 'redis://' + redisHost});
+  await redisClient.connect();
+
+  await redisClient.set(tokenId, accessToken);
+  return accessToken;
+}
--- lambda/todo-auth/test.js
+++ lambda/todo-auth/test.js
@@ -1,7 +1,7 @@
 import {handler} from "./index.js";
 
 const event = {
-    "routeKey" : "GET /items"
+    "accessToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mbyI6eyJpZCI6MSwibmFtZSI6ImxvbmdkZDkwIiwiZW1haWwiOiJsb25nLmRhbmdAZHJqb3kudm4ifSwianRpIjoiMTMzYjJiZmYtZTMyZi00YTViLTliYTQtZjg0YzQwY2NlZjk2IiwiaWF0IjoxNjk5NjA0ODkxLCJleHAiOjE2OTk2MDg0OTF9.Uf7_SxDWncKYQKxlc3nXcWAZRCRnASteEjsPFjeAKUE"
 }
 
 
